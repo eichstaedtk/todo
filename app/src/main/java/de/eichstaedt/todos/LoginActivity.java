d4ff89be7bf5585215704ddf0201e85426b264a8
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2021-05-12 19:57:53 +0200

    Add Login Activity to project

---
+++ app/src/main/java/de/eichstaedt/todos/LoginActivity.java
@@ -0,0 +1,13 @@
+package de.eichstaedt.todos;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
