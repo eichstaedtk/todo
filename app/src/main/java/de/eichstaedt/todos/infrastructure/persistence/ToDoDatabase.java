903d9424ebb96e491b1c2905f3cb803b55cb3d79
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2021-06-09 22:15:33 +0200

    Remove not needed import statements

--- app/src/main/java/de/eichstaedt/todos/infrastructure/persistence/ToDoDatabase.java
+++ app/src/main/java/de/eichstaedt/todos/infrastructure/persistence/ToDoDatabase.java
@@ -1,11 +1,8 @@
 package de.eichstaedt.todos.infrastructure.persistence;
 
-import androidx.room.AutoMigration;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
-
 import de.eichstaedt.todos.domain.ToDo;
-import de.eichstaedt.todos.domain.User;
 
 @Database(entities = {ToDo.class}, version = 7, exportSchema = false)
 public abstract class ToDoDatabase extends RoomDatabase {
