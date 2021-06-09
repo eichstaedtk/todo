4a2fa65936d16cc0c274b74e1ab2c78300743f54
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2021-06-09 21:29:21 +0200

    Refactoring readContacts with Optional

--- app/src/main/java/de/eichstaedt/todos/infrastructure/view/ContactModel.java
+++ app/src/main/java/de/eichstaedt/todos/infrastructure/view/ContactModel.java
@@ -1,6 +1,7 @@
 package de.eichstaedt.todos.infrastructure.view;
 
 import de.eichstaedt.todos.domain.ToDo;
+import java.util.Objects;
 
 public class ContactModel {
 
@@ -52,6 +53,23 @@ public class ContactModel {
 
   public String getId() {
     return id;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ContactModel that = (ContactModel) o;
+    return id.equals(that.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
   }
 
   @Override
